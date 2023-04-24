86ea72c258298e9b8035957f66cf4bf92236251e
Yahya Mansoor <125828609+YahyaMansoor@users.noreply.github.com>
2023-04-24 17:14:44 +0200

    add product filtering featuer

--- core/core/urls.py
+++ core/core/urls.py
@@ -14,10 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+"""
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+]
+"""
+
+from django.urls import path
+from myapp.views import product_list
+
+urlpatterns = [
+    path("products/", product_list, name="product_list"),
 ]
