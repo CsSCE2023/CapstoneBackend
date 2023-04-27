609a940d520a27753be4af77efdeaa38de1fa9bc
aliafzoon <aliafzoon@gmail.com>
2023-04-27 15:30:38 +0200

    fixed admin and model in mypy

--- core/myapp/admin.py
+++ core/myapp/admin.py
@@ -1,1 +1,12 @@
 # Register your models here.
+from django.contrib import admin
+
+from .models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "category", "price"]
+    search_fields = ["name", "category"]
+
+
+admin.site.register(Product, ProductAdmin)
--- core/myapp/tests/test_model.py
+++ core/myapp/tests/test_model.py
@@ -1,16 +1,15 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
+from myapp.models import Product
 
-# from myapp.models import Product
 
-
-class ProductTest(SimpleTestCase):
+class ProductTest(TestCase):
     def setup(self):
-        ...
-        # Product.objects.create(name="potato", category="vegies", price=1.85)
-        # Product.objects.create(name="fish", category="meat", price=3.00)
+        print("doing setup")
+        Product.objects.create(name="potato", category="vegies", price=1.85)
+        Product.objects.create(name="fish", category="meat", price=3.00)
 
     def test_product_created(self):
-        ...
-        # potato = Product.objects.get(name="potato")
-        # fish = Product.objects.get(name="fish")
-        # print(potato)
+        print("rinning test")
+        potato = Product.objects.get(name="potato")
+        fish = Product.objects.get(name="fish")
+        print(potato)
