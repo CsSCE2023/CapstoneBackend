da40738471cb46489af0cfc0803f7b42da14a24f
fargolk <f.karamiyar@gmail.com>
2023-04-27 23:40:52 +0200

    Upgrading Elasticsearch Spring Data 5.0.5

--- Backend/capstone/src/main/java/com/capstone/capstone/load/entities/SearchableProduct.java
+++ Backend/capstone/src/main/java/com/capstone/capstone/load/entities/SearchableProduct.java
@@ -1,23 +1,30 @@
 package com.capstone.capstone.load.entities;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
-@Document(indexName = "products")
+import java.util.Date;
+
+@Data
+@Document(indexName = "products", createIndex = false)
 public class SearchableProduct {
 
     @Id
     private String id;
 
-    @Field(type = FieldType.Text, name = "name")
+    @Field(type = FieldType.Keyword, name = "name")
     private String name;
+
+    @Field(type = FieldType.Date, name = "date")
+    private Date date;
 
     @Field(type = FieldType.Text, name = "description")
     private String description;
 
-    @Field(type = FieldType.Text, name = "shop")
+    @Field(type = FieldType.Keyword, name = "shop")
     private String shop;
 
     @Field(type = FieldType.Text, name = "address")
@@ -29,6 +36,4 @@ public class SearchableProduct {
     @Field(type = FieldType.Binary, name = "image")
     private double image;
 
-
-    // Getter and Setter
 }
--- Backend/capstone/src/main/java/com/capstone/capstone/load/repositories/SearchableProductRepository.java
+++ Backend/capstone/src/main/java/com/capstone/capstone/load/repositories/SearchableProductRepository.java
@@ -1,34 +1,21 @@
 package com.capstone.capstone.load.repositories;
 
 import com.capstone.capstone.load.entities.SearchableProduct;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.List;
 
-public interface SearchableProductRepository extends ElasticsearchRepository<SearchableProduct, String> {
+public interface SearchableProductRepository extends ElasticsearchRepository<SearchableProduct, String> , CrudRepository<SearchableProduct,String> {
 
-    List<SearchableProduct> searchSearchableProductByNameContainsOrAddressContainingOrShopContainingOrDescriptionContainingOrIgnoreCase(
-            String namePart,
-            String addressPart,
-            String shopPart,
-            String descriptionPart
+    @Query()
+    List<SearchableProduct> searchSearchableProductByNameContains(//OrAddressContainingOrShopContainingOrDescriptionContainingOrIgnoreCase(
+            String namePart//,
+            //String addressPart,
+            //String shopPart,
+            //String descriptionPart
     );
-
-    default List<SearchableProduct> searchProduct(
-            String namePart,
-            String addressPart,
-            String shopPart,
-            String descriptionPart
-    ){
-
-        return searchSearchableProductByNameContainsOrAddressContainingOrShopContainingOrDescriptionContainingOrIgnoreCase(
-                namePart,
-                addressPart,
-                shopPart,
-                descriptionPart
-        );
-
-    }
-
 
 }
